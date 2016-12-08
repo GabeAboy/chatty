dbe6e97ba6670996d5c41521b56a916f4ef6a472
Merksue <gabeaboy@gmail.com>
2016-12-08 10:08:29 -0500

    learning node server

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+
+app.listen(3000,function() {
+
+  console.log("Listening on port "+ 3000);
+
+});
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+app.post('/messages', function (req, res, next) {
+  console.log(req.body);
+  messages.push({message: req.body.message,time: new Date()});
+  res.status(200).json({messages:messages});
+});
+var messages =  [{}];
